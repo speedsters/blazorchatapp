ec81aa1576778a1ad59617e6e708e7cec5fb755d
Satya Prakash swain <satya.prakash@ecoinsight.com>
2020-08-30 13:54:26 +0530

    Add project files.

---
+++ BlazorChatSample.Server/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
+using System.IO;
+using System.Linq;
+
+namespace BlazorChat.Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews(); 
+
+            // I think this was pre-Core3.x
+            //services.AddMvc();
+            services.AddSignalR();
+
+            // not sure this is required any more
+            //services.AddResponseCompression(opts =>
+            //{
+            //    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+            //        new[] { "application/octet-stream" });
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+
+            app.UseStaticFiles();
+            app.UseBlazorFrameworkFiles();  // preview2 change
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                // SignalR endpoint routing setup
+                endpoints.MapHub<Hubs.ChatHub>(Shared.ChatClient.HUBURL);
+
+                endpoints.MapFallbackToFile("index.html");  // preview2 change
+            });
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Blazor Chat Sample
+
+[![Build Status](https://dev.azure.com/conficient/BlazorChat/_apis/build/status/conficient.BlazorChat?branchName=master)](https://dev.azure.com/conficient/BlazorChat/_build/latest?definitionId=2&branchName=master)
+
+> Now upgraded for [.NET core 3.2.0 rtm](https://devblogs.microsoft.com/aspnet/blazor-webassembly-3-2-0-now-available/) - 
+> please ensure you have installed the latest Blazor templates and VS 2019 preview.
+
+This application demonstrates the use of [SignalR](https://www.asp.net/signalr) 
+to create a [Blazor](https://blazor.net/) chat application.
+
+### Now JavaScript-Free!
+
+The app now uses the `Microsoft.AspNetCore.SignalR.Client` 
+library which is now compatible with the Mono WASM runtime. This really simplifies the 
+`ChatClient` code.
+
+Previously this sample used JavaScript SignalR client. If you want to see how the JavaScript client version worked, I've retained 
+it in [this branch](https://github.com/conficient/BlazorChat/tree/netcore-3.2.0-preview1)
+
+## Demo
+
+A demo application is available at https://BlazorChat.azurewebsites.net 
+
+### Improvements & Suggestions
+
+If you have any improvements or suggestions please submit as issues/pull requests on the Github repo.
+
+### Acknowledgements
+
+Thanks to Code-Boxx for the article https://code-boxx.com/responsive-css-speech-bubbles/ 
+that helped me create simple CSS speech bubbles that improve the layout.
